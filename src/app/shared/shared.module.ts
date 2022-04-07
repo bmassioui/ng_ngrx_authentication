d19450b691c25c4e0ef5e3e683e2c180a983d72f
bmassioui <bouchaib.massioui@gmail.com>
2022-04-07 17:30:22 +0100

    Project architecting and build shared components

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+
+@NgModule({
+    declarations: [HeaderComponent, FooterComponent],
+    imports: [],
+    exports: [HeaderComponent, FooterComponent],
+    providers: [],
+})
+
+export class SharedModule { }
+
