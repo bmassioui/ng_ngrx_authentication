f9e57167fac39751358321d41c2c1a4ed095ab28
bmassioui <bouchaib.massioui@gmail.com>
2022-04-08 12:11:20 +0100

    SignUp form

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 import { SignInComponent } from "./auth/signIn/signIn.component";
 import { SignUpComponent } from "./auth/signUp/signUp.component";
 
 @NgModule({
     declarations: [SignInComponent, SignUpComponent],
+    imports: [RouterModule],
     exports: [SignInComponent, SignUpComponent],
 })
 
