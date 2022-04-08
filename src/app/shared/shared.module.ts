43a2e5d0a6a56dbca8f73888a4ae466eef71c176
bmassioui <bouchaib.massioui@gmail.com>
2022-04-08 12:43:37 +0100

    clean up and creating ngrx files

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
-import { FooterComponent } from "./footer/footer.component";
-import { HeaderComponent } from "./header/header.component";
+
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @NgModule({
     declarations: [HeaderComponent, FooterComponent],
