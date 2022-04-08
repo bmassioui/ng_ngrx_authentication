21a02d99b03f140f42a2f43c5d258cb6415a1dea
bmassioui <bouchaib.massioui@gmail.com>
2022-04-08 17:14:01 +0100

    using reactive form

--- src/app/features/auth/components/signUp/signUp.component.ts
+++ src/app/features/auth/components/signUp/signUp.component.ts
@@ -1,7 +1,31 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { UserIterface } from "../../models/user";
 
 @Component({
     selector: 'signUp-component',
     templateUrl: './signUp.component.html'
 })
-export class SignUpComponent{}
+export class SignUpComponent implements OnInit {
+    public user: UserIterface = {};
+    public signUpFormGroup: FormGroup = new FormGroup({});
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit(): void {
+        this.initializeSignUpForm();
+    }
+
+    initializeSignUpForm(): void{
+        this.signUpFormGroup = this.formBuilder.group({
+            User
+        });
+    }
+
+    /**
+     * On SignUp form submitted
+     */
+    onSubmit(): void {
+        console.log(this.signUpFormGroup);
+    }
+}
