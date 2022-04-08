fd4df41f8382e86299edec9296a2a0e962c8f537
bmassioui <bouchaib.massioui@gmail.com>
2022-04-08 13:02:12 +0100

    Build local storage service

---
+++ src/app/features/auth/services/auth.service.ts
@@ -0,0 +1,3 @@
+// 1- SignIn
+
+// 2- SignUp
---
+++ src/app/shared/services/localStorage.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+/**
+ * Local Storage Service
+ */
+export class LocalStorageService {
+    private localStorage: Storage;
+
+    constructor() {
+        this.localStorage = window.localStorage;
+    }
+
+    /**
+     * Set new key value:string data
+     * @param key 
+     * @param value 
+     */
+    set(key: string, value: string): void {
+        this.localStorage.setItem(key, value);
+    }
+
+    /**
+     * Get value: string by its key
+     * @param key 
+     * @returns 
+     */
+    get(key: string): string | null {
+        return this.localStorage.getItem(key) ?? null;
+    }
+
+    /**
+     * Remove value by its key
+     * @param key 
+     */
+    remove(key: string): void {
+        this.localStorage.removeItem(key);
+    }
+
+    /**
+     * Clear Local storage
+     */
+    clear(): void {
+        this.localStorage.clear();
+    }
+}
