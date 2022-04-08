e122337af9e43f9e8ea03c402a3db5d8cef9cceb
bmassioui <bouchaib.massioui@gmail.com>
2022-04-08 11:44:32 +0100

    clean design

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
     declarations: [HeaderComponent, FooterComponent],
+    imports: [RouterModule],
     exports: [HeaderComponent, FooterComponent],
 })
 
