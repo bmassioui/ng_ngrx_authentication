1db4083c87be7ba5c0791c946ccaede4c4935904
bmassioui <bouchaib.massioui@gmail.com>
2022-04-09 15:30:51 +0100

    define selectors

---
+++ src/app/features/auth/store/auth.selectors.ts
@@ -0,0 +1,20 @@
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { AuthStateInterface } from "../models";
+
+export const authFeatureSelector = createFeatureSelector<AuthStateInterface>('auth')
+
+/**
+* Select isSubmitting's state
+*/
+export const isSubmitting = createSelector(
+    authFeatureSelector,
+    (authState: AuthStateInterface) => authState.isSubmitting
+)
+
+/**
+ * Select validation errors' state
+ */
+export const validationErrors = createSelector(
+    authFeatureSelector,
+    (authState: AuthStateInterface) => authState.validationErrors
+)
