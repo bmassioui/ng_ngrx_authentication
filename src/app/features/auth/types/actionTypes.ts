c041a3cfe06dfd9256d61eb8dc42ad8b982be140
bmassioui <bouchaib.massioui@gmail.com>
2022-04-09 12:33:36 +0100

    Define Action Types

---
+++ src/app/features/auth/types/actionTypes.ts
@@ -0,0 +1,8 @@
+export enum ActionTypes{
+    SIGNIN = "[Auth] SignIn",
+    SIGNIN_SUCCESS = "[Auth] SignIn Success",
+    SIGNIN_FAILURE = "[Auth] SignIn Failure",
+    SIGNUP = "[Auth] SignUp",
+    SIGNUP_SUCCESS = "[Auth] SignUp Success",
+    SIGNUP_FAILURE = "[Auth] SignUp Failure",
+}
