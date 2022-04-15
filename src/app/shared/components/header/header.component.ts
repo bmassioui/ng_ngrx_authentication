711d43d36424295173c863152c0719e4ab0cbaad
bmassioui <bouchaib.massioui@gmail.com>
2022-04-15 14:52:09 +0100

    define singin and signup functionalities

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,7 +1,25 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'header-component',
-    templateUrl:'./header.component.html'
+    templateUrl: './header.component.html'
 })
-export class HeaderComponent{}
+export class HeaderComponent {
+
+    constructor(private router: Router) { }
+
+    /**
+     * Navigate to SignIn Component
+     */
+    onSignInClick(): void {
+        this.router.navigateByUrl('/signIn');
+    }
+
+    /**
+     * Navigate to SignUp Component
+     */
+    onSignUpClick(): void {
+        this.router.navigateByUrl('/signUp');
+    }
+}
--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -1,8 +1,26 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'home-component',
     templateUrl: './home.component.html'
 })
 
-export class HomeComponent {}
+export class HomeComponent {
+
+    constructor(private router: Router) { }
+
+    /**
+     * Navigate to SignIn Component
+     */
+    onSignInClick(): void {
+        this.router.navigateByUrl('/signIn');
+    }
+
+    /**
+     * Navigate to SignUp Component
+     */
+    onSignUpClick(): void {
+        this.router.navigateByUrl('/signUp');
+    }
+}
