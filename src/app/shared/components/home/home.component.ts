e73a68a61dfef91aef51356a587690f5d1e04003
bmassioui <bouchaib.massioui@gmail.com>
2022-04-15 12:10:19 +0100

    change users icon

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -1,21 +1,8 @@
 import { Component } from "@angular/core";
-import { Router } from "@angular/router";
 
 @Component({
     selector: 'home-component',
     templateUrl: './home.component.html'
 })
 
-export class HomeComponent {
-
-    constructor(private router : Router) {
-    }
-
-    onSignInClick(): void {
-        this.router.navigate(['/signIn']);
-    }
-
-    onSignUpClick(): void {
-
-    }
-}
+export class HomeComponent {}
