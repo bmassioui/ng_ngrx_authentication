cc9a7e7babf0e9d19028bdc0a2cb484a7cdcc04b
bmassioui <bouchaib.massioui@gmail.com>
2022-04-17 10:38:25 +0100

    centralize hard coded values

--- src/app/shared/constants/sharedConstants.ts
+++ src/app/shared/constants/sharedConstants.ts
@@ -1,8 +1,20 @@
-export class SharedConstants{
+export class SharedConstants {
     public static readonly EMPTY_ROUTE_NAME = '';
     public static readonly HOME_ROUTE_NAME = 'home';
     public static readonly NOTFOUND_ROUTE_NAME = '**';
     public static readonly SIGNIN_ROUTE_NAME = 'signin';
     public static readonly SIGNUP_ROUTE_NAME = 'signup';
     public static readonly USERS_ROUTE_NAME = 'users';
+
+    /**
+     * Route Name and Route Url
+     */
+    public static readonly ROUTENAMES_ROUTEURLS: { [name: string]: string } = {
+        EMPTY_ROUTE_NAME: `/${SharedConstants.HOME_ROUTE_NAME}`,
+        HOME_ROUTE_NAME: `/${SharedConstants.HOME_ROUTE_NAME}`,
+        NOTFOUND_ROUTE_NAME: `/${SharedConstants.NOTFOUND_ROUTE_NAME}`,
+        SIGNIN_ROUTE_NAME: `/${SharedConstants.SIGNIN_ROUTE_NAME}`,
+        SIGNUP_ROUTE_NAME: `/${SharedConstants.SIGNUP_ROUTE_NAME}`,
+        USERS_ROUTE_NAME: `/${SharedConstants.USERS_ROUTE_NAME}`,
+    };
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,13 +7,12 @@ import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from "./components/home/home.component";
 import { LeftNavComponent } from "./components/leftnav/leftnav.component";
 import { NotFoundComponent } from "./components/notfound/notfound.component";
-import { PreceedStringWithSlashPipe } from "./pipes";
 import { LocalStorageService } from "./services/localStorage.service";
 
 @NgModule({
-    declarations: [HeaderComponent, ErrorListComponent, LeftNavComponent, HomeComponent, NotFoundComponent, PreceedStringWithSlashPipe],
+    declarations: [HeaderComponent, ErrorListComponent, LeftNavComponent, HomeComponent, NotFoundComponent],
     imports: [CommonModule, RouterModule],
-    exports: [HeaderComponent, ErrorListComponent, LeftNavComponent, HomeComponent, NotFoundComponent, PreceedStringWithSlashPipe],
+    exports: [HeaderComponent, ErrorListComponent, LeftNavComponent, HomeComponent, NotFoundComponent],
     providers: [LocalStorageService]
 })
 
