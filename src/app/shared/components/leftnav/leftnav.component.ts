846d819dc8fe089cd0c4010a4ab69224bc70a74d
bmassioui <bouchaib.massioui@gmail.com>
2022-04-17 11:04:52 +0100

    leftnav

--- src/app/shared/components/leftnav/leftnav.component.ts
+++ src/app/shared/components/leftnav/leftnav.component.ts
@@ -6,7 +6,7 @@ import { SharedConstants } from "../../constants";
     templateUrl: './leftnav.component.html'
 })
 
-export class LeftNavComponent{
-    public homeRouteName: SharedConstants = SharedConstants.HOME_ROUTE_NAME
-    public usersRouteName: SharedConstants = SharedConstants.USERS_ROUTE_NAME
+export class LeftNavComponent {
+    public homeRouteUrl: string = SharedConstants.ROUTENAMES_ROUTEURLS[SharedConstants.HOME_ROUTE_NAME]
+    public usersRouteUrl: string = SharedConstants.ROUTENAMES_ROUTEURLS[SharedConstants.USERS_ROUTE_NAME]
 }
--- src/app/shared/constants/sharedConstants.ts
+++ src/app/shared/constants/sharedConstants.ts
@@ -1,20 +1,20 @@
 export class SharedConstants {
-    public static readonly EMPTY_ROUTE_NAME = '';
-    public static readonly HOME_ROUTE_NAME = 'home';
-    public static readonly NOTFOUND_ROUTE_NAME = '**';
-    public static readonly SIGNIN_ROUTE_NAME = 'signin';
-    public static readonly SIGNUP_ROUTE_NAME = 'signup';
-    public static readonly USERS_ROUTE_NAME = 'users';
+    public static readonly EMPTY_ROUTE_NAME: string = '';
+    public static readonly HOME_ROUTE_NAME: string = 'home';
+    public static readonly NOTFOUND_ROUTE_NAME: string = '**';
+    public static readonly SIGNIN_ROUTE_NAME: string = 'signin';
+    public static readonly SIGNUP_ROUTE_NAME: string = 'signup';
+    public static readonly USERS_ROUTE_NAME: string = 'users';
 
     /**
      * Route Name and Route Url
      */
     public static readonly ROUTENAMES_ROUTEURLS: { [name: string]: string } = {
-        EMPTY_ROUTE_NAME: `/${SharedConstants.HOME_ROUTE_NAME}`,
-        HOME_ROUTE_NAME: `/${SharedConstants.HOME_ROUTE_NAME}`,
-        NOTFOUND_ROUTE_NAME: `/${SharedConstants.NOTFOUND_ROUTE_NAME}`,
-        SIGNIN_ROUTE_NAME: `/${SharedConstants.SIGNIN_ROUTE_NAME}`,
-        SIGNUP_ROUTE_NAME: `/${SharedConstants.SIGNUP_ROUTE_NAME}`,
-        USERS_ROUTE_NAME: `/${SharedConstants.USERS_ROUTE_NAME}`,
+        '': `/${SharedConstants.HOME_ROUTE_NAME}`,
+        'home': `/${SharedConstants.HOME_ROUTE_NAME}`,
+        '**': `/${SharedConstants.NOTFOUND_ROUTE_NAME}`,
+        'signin': `/${SharedConstants.SIGNIN_ROUTE_NAME}`,
+        'signup': `/${SharedConstants.SIGNUP_ROUTE_NAME}`,
+        'users': `/${SharedConstants.USERS_ROUTE_NAME}`,
     };
 }
